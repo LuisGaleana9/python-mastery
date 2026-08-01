6021a16c2c8c50160a0158c9fb70bef66fa38090
LuisGaleana9 <luisgaleana99@gmail.com>
2026-07-31 23:34:42 -0600

    refactor: improve ATM project structure and code organization

--- 00_mini_projects/mini_cajero.py
+++ 00_mini_projects/mini_cajero.py
@@ -1,32 +1,85 @@
 #Ejercicio de Cajero automatico con funciones basicas. #1
+#Actualizacion de Cajero automatico con buenas practicas.
 
-opcion = 0
-saldo = 0
-
-while opcion != 4: 
+def menu():
+    """
+    Desplega el menu para el usuario.
+    """
     print("\nBienvenido a tu cajero automatico.\n")
     print("1.- Consultar saldo")
     print("2.- Depositar saldo")
     print("3.- Retirar dinero")
     print("4.- Salir")
-    opcion = int(input("Ingresa una opcion: "))
 
-    match opcion:
-        case 1: 
-            print(f"Tu saldo es de {saldo}")
-        case 2:
-            saldo += int(input("Cuanto saldo deseas ingresar a tu cuenta de banco: "))
-        case 3:
-            if saldo == 0:
-                print("No tienes saldo.")
-            else:
-                print(f"Tu saldo disponible es de {saldo}")
-                retiro = int(input("Cuanto dinero deseas retirar?: "))
-                if retiro > saldo:
-                    print(f"No tienes ese saldo disponible. Tienes {saldo}")
-                else:
-                    saldo -= retiro
-        case 4: 
-            print("Hasta luego.")
-        case _:
-            print("Elige una opcion valida.")
+def consultar_saldo(saldo):
+    """
+    Funcion para consultar el saldo disponible en la cuenta
+    """
+    print(f"Tu saldo es de {saldo}")
+
+def depositar_saldo(saldo):
+    """
+    Funcion para depositar saldo 
+    Recibe saldo,deposito.
+    """
+    deposito = int(input("Cuanto saldo deseas ingresar a tu cuenta de banco: "))
+    return saldo + deposito
+
+def retirar_saldo(saldo):
+    """
+    Funcion para retirar saldo
+    Recibe saldo,retiro
+    """
+    retiro = int(input("Cuanto dinero deseas retirar?: "))
+    if puede_retirar(saldo,retiro):
+        return saldo - retiro
+    return saldo
+
+def verificar_saldo(saldo):
+    """
+    Funcion que recibe saldo para verificar si tienes saldo mayor a 0
+    """
+    if saldo == 0:
+        print("No tienes saldo disponible.")
+        return False
+    return True
+
+def puede_retirar(saldo, retiro):
+    """
+    Funcion que valida si tu saldo es mayor a la cantidad que quieres retirar
+    """
+    if saldo < retiro:
+        print("No tienes suficiente saldo para retirar.")
+        return False
+    else:
+        return True
+    
+
+def main():
+    """
+    Funcion main que corre todo el programa.
+    """
+    opcion = 0
+    saldo = 0
+
+    while opcion != 4: 
+        menu()
+        opcion = int(input("Ingresa una opcion: "))
+
+        match opcion:
+            case 1: 
+                consultar_saldo(saldo)
+            case 2:
+                saldo = depositar_saldo(saldo)
+                consultar_saldo(saldo)
+            case 3:
+                if verificar_saldo(saldo):
+                    saldo = retirar_saldo(saldo)
+                    consultar_saldo(saldo)
+            case 4: 
+                print("Hasta luego.")
+            case _:
+                print("Elige una opcion valida.")
+
+if __name__ == "__main__":
+    main()
