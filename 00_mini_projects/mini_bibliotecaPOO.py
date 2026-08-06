4a960867e5950b4b4c8006297f22b59818cd5ffa
LuisGaleana9 <luisgaleana99@gmail.com>
2026-08-05 19:35:09 -0600

    feat: implement object-oriented library management system

--- 00_mini_projects/mini_bibliotecaPOO.py
+++ 00_mini_projects/mini_bibliotecaPOO.py
@@ -29,6 +29,114 @@ class Libro:
 class Biblioteca:
 
     def __init__(self):
-        pass
+        self.biblioteca = []
 
-    def agregar_libro()
+    def vacia(self):
+        i = 0
+        for libro in self.biblioteca:
+            i+=1
+
+        if i > 0:
+            return True
+        else:
+            print("La biblioteca esta vacia.")
+            return False
+        
+    def agregar_libro(self, Libro):
+        (self.biblioteca).append(Libro)
+
+    def eliminar_libro(self, titulo):
+        for libro in self.biblioteca:
+            if libro.titulo == titulo:
+                (self.biblioteca).remove(libro)
+                print(f"El libro {libro.titulo} se ha eliminado de la biblioteca.")
+
+    def buscar_libro(self, titulo):
+        for libro in self.biblioteca:
+            if libro.titulo == titulo:
+                libro.mostrar_info()
+
+    def prestar_libro(self, titulo):
+        for libro in self.biblioteca:
+            if libro.titulo == titulo:
+                libro.prestar()
+
+    def devolver_libro(self, titulo):
+        for libro in self.biblioteca:
+            if libro.titulo == titulo:
+                libro.devolver()
+
+    def ver_biblioteca(self):
+        if self.vacia():
+            i = 1
+            for libro in self.biblioteca:
+                print(f"Libro #{i}")
+                libro.mostrar_info()
+                i += 1
+
+
+def menu():
+    print("\n------------MENU-------------")
+    print("1.-Agregar libro")
+    print("2.-Ver biblioteca")
+    print("3.-Eliminar libro")
+    print("4.-Buscar un libro")
+    print("5.-Prestar un libro")
+    print("6.-Devolver un libro")
+    print("7.-Salir\n")
+    return int(input("Ingresa una opcion: "))
+
+def agregar_libro(biblioteca):
+    titulo = input("Ingresa el nombre del libro: ")
+    autor = input("Ingresa el nombre del autor: ")
+
+    libro = Libro(titulo,autor)
+    biblioteca.agregar_libro(libro)
+
+def eliminar_libro(biblioteca):
+    if biblioteca.vacia():
+        titulo = input("Ingresa el nombre del libro que deseas eliminar: ")
+        biblioteca.eliminar_libro(titulo)
+
+def buscar_libro(biblioteca):
+    if biblioteca.vacia():
+        titulo = input("Ingresa el nombre del libro que buscas: ")
+        biblioteca.buscar_libro(titulo)
+
+def prestar_libro(biblioteca):
+    if biblioteca.vacia():
+        titulo = input("Ingresa el nombre del libro que quieres prestar: ")
+        biblioteca.prestar_libro(titulo)
+
+def devolver_libro(biblioteca):
+    if biblioteca.vacia():
+        titulo = input("Ingresa el nombre del libro que vas a devolver")
+        biblioteca.devolver_libro(titulo)
+
+def main():
+
+    biblioteca = Biblioteca()
+    eleccion = 0
+
+    while eleccion != 7:
+       
+        eleccion = menu()
+
+        match eleccion:
+            case 1:
+                agregar_libro(biblioteca)
+            case 2:
+                biblioteca.ver_biblioteca()
+            case 3:
+                eliminar_libro(biblioteca)
+            case 4:
+                buscar_libro(biblioteca)
+            case 5:
+                prestar_libro(biblioteca)
+            case 6:
+                devolver_libro(biblioteca)
+
+        
+
+if __name__ == "__main__":
+    main()
